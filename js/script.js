a1be15e3cca054f940c1765c87512c3749312151
phbrandi <pedrohab1@al.insper.edu.br>
2024-11-08 10:15:50 -0300

    apagando comentários

--- js/script.js
+++ js/script.js
@@ -1,30 +1,5 @@
 
 
-// document.addEventListener('DOMContentLoaded', function() {
-
-//     text = document.querySelector('input[type="text"]')    
-
-//     submit = document.querySelector('button[type="submit"]')
-
-//     ul = document.querySelector('ul')
-
-//     submit.addEventListener('click', function(event){
-
-//         li = document.querySelector('li')
-//         li_prime = li.cloneNode(true)
-//         label = document.querySelector('label')
-//         label_prime = label.cloneNode(true)
-//         span = document.querySelector('span')
-//         span.innerHTML = text.value
-//         ul.insertBefore(li_prime)
-//         ul.appendChild(li_prime)
-
-//         event.preventDefault()
-
-//     })
-
-// })
-
 document.addEventListener('DOMContentLoaded', function() {
     const textInput = document.querySelector('input[type="text"]');
     const submitButton = document.querySelector('button[type="submit"]');
@@ -50,9 +25,8 @@ document.addEventListener('DOMContentLoaded', function() {
             li.appendChild(label);
             ul.appendChild(li);
             
-            textInput.value = '';  // Limpa o campo de entrada
-
-            // Rola para o último item adicionado
+            textInput.value = '';  
+           
             ul.scrollTop = ul.scrollHeight;
         }
     });
